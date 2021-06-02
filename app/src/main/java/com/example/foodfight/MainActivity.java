5e7ad02958c4e1f6f5c48d20ff1ec020a0e12438
pHr01d <pro20006@byui.edu>
2021-06-02 18:46:53 -0500

    Adding comment

--- app/src/main/java/com/example/foodfight/MainActivity.java
+++ app/src/main/java/com/example/foodfight/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /*Kaleb can edit */
-    /* Tom Edits*/
+
+    // This is my contribution
 }
