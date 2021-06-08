915fbf1f8536c8c53483a15d5fdb5b70d744210b
pHr01d <pro20006@byui.edu>
2021-06-08 17:44:59 -0500

    Add basic layouts for activities

---
+++ app/src/main/java/com/example/foodfight/DayPicker.java
@@ -0,0 +1,14 @@
+package com.example.foodfight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DayPicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_picker);
+    }
+}
---
+++ app/src/main/java/com/example/foodfight/Meals.java
@@ -0,0 +1,14 @@
+package com.example.foodfight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Meals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meals);
+    }
+}
