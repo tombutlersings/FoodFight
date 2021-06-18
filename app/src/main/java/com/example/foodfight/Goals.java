52bb99c980e870b51ea58825e2c50018da2c8274
pHr01d <pro20006@byui.edu>
2021-06-18 17:55:35 -0500

    Added Goals class to hold goal values for global access

---
+++ app/src/main/java/com/example/foodfight/Goals.java
@@ -0,0 +1,11 @@
+package com.example.foodfight;
+
+public class Goals {
+    private double GoalDaily;
+    private double GoalWeekly;
+
+    public double getDaily() { return GoalDaily; }
+    public void setDaily(double goalDaily) { GoalDaily = goalDaily; }
+    public double getWeekly() { return GoalWeekly; }
+    public void setWeekly(double goalWeekly) { GoalWeekly = goalWeekly; }
+}
