24617629491f5b639c6875f5fb7ee9894f15e079
Caleb P. Nwokocha <nwo20001@byui.edu>
2021-06-20 23:01:08 -0500

    Calendar activity update v1.0

--- app/src/main/java/com/example/foodfight/Calendar.java
+++ app/src/main/java/com/example/foodfight/Calendar.java
@@ -3,16 +3,14 @@ package com.example.foodfight;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class Calendar extends AppCompatActivity {
 
-    // Define the variable of CalendarView type
-    // and TextView type;
+    // Create CalendarView and TextView objects, calender and date_view.
     CalendarView calender;
-    TextView date_view;
+    TextView date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,39 +18,9 @@ public class Calendar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // By ID we can use each component
-        // which id is assign in xml file
-        // use findViewById() to get the
-        // CalendarView and TextView
-        calender = (CalendarView)findViewById(R.id.calendar_view);
-        date_view = (TextView) findViewById(R.id.date_view);
+        // Define view objects as their view ID.
+        calender = (CalendarView)findViewById(R.id.calendar);
+        date = (TextView) findViewById(R.id.date);
 
-        // Add Listener in calendar
-        calender.setOnDateChangeListener(
-                new CalendarView.OnDateChangeListener() {
-
-                            @Override
-                            // In this Listener have one method
-                            // and in this method we will
-                            // get the value of DAYS, MONTH, YEARS
-                            public void onSelectedDayChange(
-                                    @NonNull CalendarView view,
-                                    int year,
-                                    int month,
-                                    int dayOfMonth)
-                            {
-
-                                // Store the value of date with
-                                // format in String type Variable
-                                // Add 1 in month because month
-                                // index is start with 0
-                                String Date
-                                        = dayOfMonth + "-"
-                                        + (month + 1) + "-" + year;
-
-                                // set this date in TextView for Display
-                                date_view.setText(Date);
-                            }
-                        });
     }
 }
