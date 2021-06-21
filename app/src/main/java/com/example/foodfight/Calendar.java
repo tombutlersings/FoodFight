d5b086c508ebb066cc970c8ab03d65d5956db4b4
Caleb P. Nwokocha <nwo20001@byui.edu>
2021-06-20 23:07:49 -0500

    added listener to calendar

--- app/src/main/java/com/example/foodfight/Calendar.java
+++ app/src/main/java/com/example/foodfight/Calendar.java
@@ -22,5 +22,32 @@ public class Calendar extends AppCompatActivity {
         calender = (CalendarView)findViewById(R.id.calendar);
         date = (TextView) findViewById(R.id.date);
 
+        // Add Listener in calendar
+        calender.setOnDateChangeListener(
+                new CalendarView.OnDateChangeListener() {
+
+                            @Override
+                            // In this Listener have one method
+                            // and in this method we will
+                            // get the value of DAYS, MONTH, YEARS
+                            public void onSelectedDayChange(
+                                    @NonNull CalendarView view,
+                                    int year,
+                                    int month,
+                                    int dayOfMonth)
+                            {
+
+                                // Store the value of date with
+                                // format in String type Variable
+                                // Add 1 in month because month
+                                // index is start with 0
+                                String Date
+                                        = dayOfMonth + "-"
+                                        + (month + 1) + "-" + year;
+
+                                // set this date in TextView for Display
+                                date.setText(Date);
+                            }
+                        });
     }
 }
