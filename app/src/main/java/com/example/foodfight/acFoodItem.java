dfaa3fea4b8f65ad07e1cf111bb61e5ecdcbdd1a
tombutlersings <but20021@byui.edu>
2021-06-25 16:59:51 -0700

    acFoodItem Activity and Class created

--- app/src/main/java/com/example/foodfight/acFoodItem.java
+++ app/src/main/java/com/example/foodfight/acFoodItem.java
@@ -1,4 +1,14 @@
 package com.example.foodfight;
 
-public class acFoodItem {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class acFoodItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac_food_item);
+    }
 }
