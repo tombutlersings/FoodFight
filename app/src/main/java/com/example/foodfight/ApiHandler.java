8f70cc3df3ad7811f4bc495e75559ea6f5c1d181
tombutlersings <but20021@byui.edu>
2021-06-28 17:25:19 -0700

    acFoodItem Sudocode

--- app/src/main/java/com/example/foodfight/ApiHandler.java
+++ app/src/main/java/com/example/foodfight/ApiHandler.java
@@ -1,6 +1,8 @@
 package com.example.foodfight;
 
 // TODO extends Thread or Runnable? -Tom 8-jun-21
+// TODO: will receive Strings from AddFoodHandler
+// This class may also handle calls to the dictionary?
 
 public class ApiHandler {
 
--- app/src/main/java/com/example/foodfight/acAddFood.java
+++ app/src/main/java/com/example/foodfight/acAddFood.java
@@ -15,7 +15,18 @@ public class acAddFood extends AppCompatActivity {
         setContentView(R.layout.activity_add_food);
     }
 
-    // TODO: Methods
-    // TODO:
+    // TODO: METHOD search for food
+    // Todo: 2 threads (one for DB and one for Api)
+        //top five results from DB
+        //next five results from api
+            //todo: method to call apiHandlerClass
+                //provides the search string
+
+
+            // first internal search
+    // TODO: METHOD display results
+        // TODO: Creating Connection between results and buttons
+        // TODO: Click on the result to bring that food up on the food item page.
+            // pass foodItem data to the acAddFood and AddFoodHandler screen
 
 }
