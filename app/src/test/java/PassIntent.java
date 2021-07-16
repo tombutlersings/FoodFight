afaa769457ec604264a431224317e7717724c793
KalebPulley <pul21001@byui.edu>
2021-07-15 19:36:05 -0600

    some comments added to acUserProfile.java

---
+++ app/src/test/java/PassIntent.java
@@ -0,0 +1,5 @@
+public interface PassIntent {
+    public void getIntent();//get intent information from previous activity.
+    public void sendIntent();//send intent information to Next Activity.
+
+}
