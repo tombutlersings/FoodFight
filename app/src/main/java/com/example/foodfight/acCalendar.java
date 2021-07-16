a0d9f34134f718dfc0bb46799d9baed9c03558d2
Caleb P. Nwokocha <nwo20001@byui.edu>
2021-07-16 15:12:03 -0500

    Added date selection constraint to calendar activity

--- app/src/main/java/com/example/foodfight/acCalendar.java
+++ app/src/main/java/com/example/foodfight/acCalendar.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import androidx.core.view.GestureDetectorCompat;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -28,6 +29,26 @@ public class acCalendar extends AppCompatActivity {
         CalendarView calendarView = findViewById(R.id.calendar);
         TextView calendarDateView = findViewById(R.id.calendar_date);
 
+        // Add calendar constraint for maximum and minimum date
+        // User can select a date one year earlier or a date
+        // one year in the future.
+        Calendar cal1 = null;
+        Calendar cal2 = null;
+        long afterOneYearsinMilli = 0;
+        long beforeOneYearsinMilli = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            // Max
+            cal1 = Calendar.getInstance();
+            cal1.add(Calendar.YEAR, 1);
+            afterOneYearsinMilli = cal1.getTimeInMillis();
+
+            // Min
+            cal2 = Calendar.getInstance();
+            cal2.add(Calendar.YEAR, -1);
+            beforeOneYearsinMilli = cal2.getTimeInMillis();
+        }
+        calendarView.setMaxDate(afterOneYearsinMilli);
+        calendarView.setMinDate(beforeOneYearsinMilli);
 
         // Add Listener in calendar
         // In this Listener have one method
@@ -49,7 +70,6 @@ public class acCalendar extends AppCompatActivity {
                     // set this date in TextView for Display
                     calendarDateView.setText(calendarDate);
                 });
-
     }
 
     public void btnDone(View view) {
