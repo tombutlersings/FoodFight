602fee238cbd31c9a3c7a520efc667c3d3387b61
pHr01d <pro20006@byui.edu>
2021-07-17 13:23:50 -0500

    Cleanup in DatabaseHandler.java, MealItem.java

--- app/src/main/java/com/example/foodfight/MealItem.java
+++ app/src/main/java/com/example/foodfight/MealItem.java
@@ -1,19 +1,16 @@
 package com.example.foodfight;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MealItem {
     public Integer ID;
     public String date;
-    private String name; //from meals enum
-    //private int totalCalories; // math total for calories in the food lists of the meals
-    public ArrayList<FoodItem> foodItems = new ArrayList<>();
-    public  ArrayList<Float> qty = new ArrayList<>();
+    private String name;
+    public ArrayList<FoodItem> foodItems;
+    public  ArrayList<Float> qty;
 
 
-    // Constructor for storing a food item
-    // (object of the FoodItem class) in
+    // Constructor for storing a food item (object of the FoodItem class) in
     // foodItems array.
     public MealItem(Integer ID, String date, String name, ArrayList<FoodItem> foods, ArrayList<Float> qty) {
         this.name = name;
@@ -21,8 +18,6 @@ public class MealItem {
         this.ID = ID;
         this.date = date;
         this.qty = qty;
-        //foodItems.add(item1);
-        //foodItems.add(item2);
     }
 
     public String getMealName() {return name;}
@@ -30,12 +25,6 @@ public class MealItem {
     // Find the total calories of all food.
     public float getTotalCalories() {
         float totalCalories = 0;
-
-        // Loop through foodItems array and
-        // get the sum of calories of all food.
-//        for (FoodItem food : foodItems) {
-//            totalCalories += food.getCalories();
-//        }
         for (int i = 0; i < foodItems.size(); i++) {
             //tempCalories += servings * fooditem.getCalories();
             FoodItem fooditem = foodItems.get(i);
