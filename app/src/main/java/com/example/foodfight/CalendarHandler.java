b8919ffcd06d973c3e90457e9a0c38d192832610
tombutlersings <but20021@byui.edu>
2021-07-17 18:29:13 -0400

    CalendarHandler updated less comments

--- app/src/main/java/com/example/foodfight/CalendarHandler.java
+++ app/src/main/java/com/example/foodfight/CalendarHandler.java
@@ -10,17 +10,6 @@ import androidx.annotation.RequiresApi;
 import java.util.Date;
 import java.util.Locale;
 
-/**
-* Tom 8Jun21:
-* This is to handle calling up date objects to the
- * TODO is this going to be in the Presenter or Model layer?
- *
- * Caleb 8Jun21:
- * CALENDAR HANDLER
- * CalendarHandler class encapsulate the day, month, year, and date
- * values from acCalendar activity. It also encapsulate android
- * system date value.
- */
 public class CalendarHandler {
 
     private acCalendar calendarActivity = new acCalendar();
@@ -40,10 +29,6 @@ public class CalendarHandler {
     public String getCalendarDate() {
         return calendarActivity.calendarDate;
     }
-
-    // Caleb 8Jun21:
-    // GET SYSTEM DATE
-    // getSystemDate() returns current android system date value.
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public String getSystemDate() {
